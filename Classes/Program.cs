38b9ba8dc393af5551c46dc9f56f75762e0a0955
GustavoTagli <taglisouza.0304@hotmail.com>
2022-07-20 23:45:07 -0300

    @GustavoTagli[exercise_Classes]

---
+++ Classes/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Classes.Src;
+
+namespace Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Monstro Barney = new Monstro("Barney", "Burgundy", 2, 2);
+            Monstro Sullivan = new Monstro("Sullivan", "Blue", 2, 2);
+            Monstro Wazowski = new Monstro("Wazowski", "Green", 2, 1);
+            Monstro FourArms = new Monstro("Four Arms", "Red", 4, 4);
+
+            Console.WriteLine(Barney.ToString());
+            Console.WriteLine(Sullivan.ToString());
+            Console.WriteLine(Wazowski.ToString());
+            Console.WriteLine(FourArms.ToString());
+            Console.WriteLine(FourArms.Grunt());
+        }
+    }
+}
---
+++ Classes/Src/Monstro.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes.Src
+{
+    internal class Monstro
+    {
+        string Name { get; set; }
+        string Color { get; set; }
+        int NmbrArms { get; set; }
+        int NmbrEyes { get; set; }
+
+        public Monstro(string name, string color, int nmbrArms, int nmbrEyes)
+        {
+            Name = name;
+            Color = color;
+            NmbrArms = nmbrArms;
+            NmbrEyes = nmbrEyes;
+        }
+
+        public string Grunt()
+        {
+            return "Grrrrrww!!";
+        }
+
+        public override string ToString()
+        {
+            return $"Eu sou o {Name}, tenho a cor {Color}, tenho {NmbrArms} braços e {NmbrEyes} olhos.";
+        }
+    }
+}
